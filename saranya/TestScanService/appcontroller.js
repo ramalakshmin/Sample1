23eb963280a6cdb2e4b105ce1ce6f59708ea7c4b
ramalakshmin <ramu1512@gmail.com>
2015-10-28 17:30:07 +0530

    test

---
+++ saranya/TestScanService/appcontroller.js
@@ -0,0 +1,26 @@
+﻿var App = angular.module('Test', ['popupdlg']);
+app.controller('appcontroller',function ($Scope,popupdlg)
+{
+    var model=popupdlg.Dialog(
+        {
+            id: "K7",
+            width: "1000",
+            height: "235",
+            title: "Info Window",
+            footerActions: '<button ng-click="ClosePopup()" class="oAction">Ok</button><button ng-click="AlertPopup()" class="oAction">Check</button>'
+        });
+    $Scope.show_dialog()=function()
+    {
+        model.open();
+    }
+    model.$Scope.AlertPopup() = function ()
+    {
+        alert('Pop up window is loaded');
+    }
+    model.$Scope.ClosePopup() = function ()
+    {
+        model.close();
+    }
+});
+
+
